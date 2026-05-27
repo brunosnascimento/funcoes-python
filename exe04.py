adf2dd6d87f3e00af6c77ddb88a13182de43da1e
Bruno Nascimento <matbsn@gmail.com>
2026-05-27 20:04:06 -0300

    modifed exe04

---
+++ exe04.py
@@ -0,0 +1,13 @@
+def maior_numero(a, b):
+    if a > b:
+        print(f"o número {a} é maior")
+    else:
+        print(f"o número {b} é maior")
+
+# Exemplo:
+
+maior_numero(a=3, b=9)
+maior_numero(b=9, a=3)
+
+def main():
+    # aqui meu code principal
